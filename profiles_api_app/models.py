1c64aff5190be54d6a7aba5b3722d72e6f65691d
Kushal Bergkamp <kushalj001@gmail.com>
2018-02-02 23:34:15 +0530

    added user models and made migrations

--- profiles_api_app/models.py
+++ profiles_api_app/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractBaseUser
+# the AbstractBaseUser is the base of the django's standard user model
+# we are going to override the default user model that django provides us with our own customized user model
+from django.contrib.auth.models import PermissionsMixin
+# to include all the permissions for our customized user model that the base model would otherwise have
+from django.contrib.auth.models import BaseUserManager
+
+class UserProfileManager(BaseUserManager):
+    """ Helps django work with our custom user model """
+
+    def create_user(self,email,name,password=None):
+        """ creates a new user profile object. """
+
+        if not email:  # checks whether  email field  has been filled
+            raise ValueError('Users must have a valid email address')
+
+        email=self.normalize_email(email) # normalizes ~> converts all chars to lowercase to maintain uniformity
+
+        user=self.model(email=email,name=name)#creates a new user object for the custom profile we have made
+        # self.model implies the model it is linked to , which in this case is UserProfile model
+        # the link in the connection is ~>BaseUserManager->AbstractBaseUser(UserProfile model)
+
+        user.set_password(password) # converts password to #####
+        user.save(using=self.db) # saves in database
+
+        return user
+
+    def create_superuser(self,name,email,password):
+        """ creates and saves a new superuser(admin privileges) with given details ."""
+
+        user=self.create_user(email,name.password)
+
+        user.is_superuser=True
+        user.is_staff=True
+
+        user.save(using=self.db)
+
+        return user
+
+
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    # Represents a user profile in our system
+
+    email=models.EmailField(max_length=120,unique=True)
+    name=models.CharField(max_length=120)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+
+    objects=UserProfileManager() # helps in managing the user profiles and gives us some extra functionality like
+                                 # creating an admin user or a regular user.
+                                 # moreover it's required when we are overwriting the base user model
+
+    USERNAME_FIELD='email' # the standarddjango model has a field called USERNAME_FIELD which acts as the username or
+                           # a handle whic is used to login etc.We have set that field to email instead of a custom username.
+
+    REQUIRED_FIELDS=['name'] # fields that the user must fill in to register
+
+    def get_full_name(self):
+        """ used to get a user's full name."""
+
+        return self.name
+
+    def get_short_name(self):
+        """ used to get user's short name """
+
+        return self.name
+
+    def __str__(self):
+        """ django uses this when it needs to convert the object to a string """
+        return self.email
--- profiles_rest_project/settings.py
+++ profiles_rest_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',               # including the rest framework in our installed apps
+    'rest_framework.authtoken',
+    'profiles_api_app',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL='profiles_api_app.UserProfile' # tells django to use this model as default and not the original one
